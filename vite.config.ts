e65036fd5ef403e6ece99412672a849c9bb3b529
Marcus Neto <163374075+mnetoo@users.noreply.github.com>
2025-12-11 20:28:17 -0300

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LojaVirtual', // Substitua pelo caminho desejado
+  base: '/LojaVirtual/', // Substitua pelo caminho desejado
   plugins: [
     react(),
     tailwindcss(),
