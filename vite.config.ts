e579cb6a9ec0c1ff7bbba4a91379821015f2e9f3
Marcus Neto <163374075+mnetoo@users.noreply.github.com>
2025-12-11 20:25:36 -0300

    Remove trailing slash from base path in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/LojaVirtual/', // Substitua pelo caminho desejado
+  base: '/LojaVirtual', // Substitua pelo caminho desejado
   plugins: [
     react(),
     tailwindcss(),
