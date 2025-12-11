7ceaf2036cb82ab085e060dd57dd48634135d2ac
Marcus Neto <marcusnetoo@outlook.com>
2025-12-11 20:58:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Substitua pelo caminho desejado
+  base: '/LojaVirtual/', // Substitua pelo caminho desejado
   plugins: [
     react(),
     tailwindcss(),
